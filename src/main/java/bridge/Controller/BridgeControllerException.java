99acc2ce6821be87d6437eef5748497f15a66b41
Yeonwoo <yoanwoo0227@gmail.com>
2023-09-09 21:43:31 +0900

    feat : BridgeControllerException에 이동할 칸을 입력받았을때 U또는 D가 아니면 예외를 던지는 alphabetCheck() 구현
    입력받은 string이 D 또는 U가 아니면 IllegalArgumentException예외를 던진다. 테스트를 통해 잘 작동함을 확인했다.

--- src/main/java/bridge/Controller/BridgeControllerException.java
+++ src/main/java/bridge/Controller/BridgeControllerException.java
@@ -14,10 +14,16 @@ public class BridgeControllerException {
         }
     }
 
-    public static void rangeCheck(String inputString){
+    public static void rangeCheck(String inputString){ //입력받은 숫자가 범위조건을 만족하는지 체크하는 함수
         int inputNumber=Integer.parseInt(inputString);
         if(inputNumber< Constants.BRIDGE_MIN_NUMBER||inputNumber>Constants.BRIDGE_MAX_NUMBER){
             throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+        }
+    }
+    
+    public static void alphabetCheck(String inputString){
+        if(!inputString.equals("D")&&!inputString.equals("U")){
+            throw new IllegalArgumentException("[ERROR] 이동할 칸은 U 또는 D로 입력해주세요");
         }
     }
 }
--- src/test/java/bridge/Controller/BridgeControllerExceptionTest.java
+++ src/test/java/bridge/Controller/BridgeControllerExceptionTest.java
@@ -6,25 +6,30 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
 class BridgeControllerExceptionTest {
     @DisplayName("입력받은 다리의 길이가 숫자 형식이 아니면 예외처리")
     @Test
     void 예외_테스트1() {
-        try {
-            BridgeControllerException.numberCheck("5a");
-        } catch (RuntimeException e) {
-            Assertions.assertEquals("[ERROR] 다리 길이는 숫자 형식이어야 합니다.", e.getMessage());
-        }
+        Assertions.assertThrows(NumberFormatException.class,()->{
+            BridgeControllerException.rangeCheck("3a");
+        });
 
     }
     @DisplayName("입력받은 다리의 길이가 3이상 20이하의 범위를 벗어날 때")
     @Test
     void 예외_테스트2() {
-        try {
-            BridgeControllerException.rangeCheck("25");
-        } catch (IllegalArgumentException e) {
-            Assertions.assertEquals("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.", e.getMessage());
-        }
+        Assertions.assertThrows(IllegalArgumentException.class,()->{
+            BridgeControllerException.rangeCheck("2");
+        });
+    }
+
+    @DisplayName("입력받은 이동할 칸이 U또는 D로 입력되지 않았다면 예외처리")
+    @Test
+    void 예외_테스트3() {
+        Assertions.assertThrows(IllegalArgumentException.class,()->{
+            BridgeControllerException.alphabetCheck("u");
+        });
 
     }
 }
