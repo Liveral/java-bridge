5c03153e4819598848c585aa33a20742366bb5fd
Yeonwoo <yoanwoo0227@gmail.com>
2023-09-09 23:17:28 +0900

    Fix : BridgeController 클래스 내 compareToBridge() 수정
    기존에는 OutputView로 바로 보내는 형식이였는데 이렇게 되면 OutputView에서 값을 가공해야하는 문제가 생기므로 Bridge클래스로 넘기는 형식으로 바꿨다.

--- src/main/java/bridge/Controller/BridgeController.java
+++ src/main/java/bridge/Controller/BridgeController.java
@@ -34,13 +34,14 @@ public class BridgeController {
     public void userMove(){
         String moving=inputView.readMoving();
         BridgeControllerException.movingValidateCheck(moving); //이동할 칸 입력값 예외처리
-        
+        compareToBridge(moving);
     }
     public void compareToBridge(String move){
         int moveIndex=bridge.getCheckIndex(); //이번에 사용자가 이동할 칸의 인덱스 값
         if(move.equals(bridge.getMadeBridge().get(moveIndex))){
-           outputView.printMap(true, moveIndex); //이동할 칸과 비교해서 같으면 통과를 뜻하는 true와 moveIndex값을 OutputView클래스의 printMap에 전달한다.
+           bridge.setCheckedBridge("O"); //이동할 칸과 비교해서 같으면 통과를 뜻하는 "O"를 Bridge클래스의 setCheckedBridge에 전달한다.
+            return;
         }
-        outputView.printMap(false,moveIndex); ////다르면 실패를 뜻하는 false와 moveIndex값을 OutputView클래스의 printMap에 전달한다.
+        bridge.setCheckedBridge("X"); ////다르면 실패를 뜻하는 "X"를 Bridge클래스의 setCheckedBridge에 전달한다.
     }
 }
