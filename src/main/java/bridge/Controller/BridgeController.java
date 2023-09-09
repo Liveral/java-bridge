1f70184cf57f0ea762cfbed14c591f25b4b0a4c3
Yeonwoo <yoanwoo0227@gmail.com>
2023-09-09 22:14:36 +0900

    Fix : BridgeController에 있던 예외처리 함수들 BridgeControllerException클래스로 옮김
    BridgeController클래스에 너무 많은 메소드들이 있으면 안될것 같다는 생각에입력에 대한 예외처리는 BridgeControllerException클래스로 싹 다 옮겨 주었다.

--- src/main/java/bridge/Controller/BridgeController.java
+++ src/main/java/bridge/Controller/BridgeController.java
@@ -23,36 +23,22 @@ public class BridgeController {
 
     public void bridgeMake(){
         String bridgeSize=inputView.readBridgeSize();
-        bridgeValidateCheck(bridgeSize); //다리 길이 입력값 예외처리
+        BridgeControllerException.bridgeValidateCheck(bridgeSize); //다리 길이 입력값 예외처리
         BridgeMaker bridgeMaker=new BridgeMaker(bridgeNumberGenerator);
         bridge.setBridgeLength(Integer.parseInt(bridgeSize));
         bridge.setMadeBridge(bridgeMaker.makeBridge(Integer.parseInt(bridgeSize)));
     }
 
-    public void bridgeValidateCheck(String input){
-        try {
-            BridgeControllerException.numberCheck(input);
-            BridgeControllerException.rangeCheck(input);
-        }
-        catch (NumberFormatException e){
-            System.out.println(e.getMessage());
-        }catch (IllegalArgumentException e){
-            System.out.println(e.getMessage());
-        }
-    }
 
     public void userMove(){
         String moving=inputView.readMoving();
-        movingValidateCheck(moving); //이동할 칸 입력값 예외처리
-
+        BridgeControllerException.movingValidateCheck(moving); //이동할 칸 입력값 예외처리
+        String a=bridge.getMadeBridge().get(0);
     }
 
-    public void movingValidateCheck(String input){
-        try {
-            BridgeControllerException.alphabetCheck(input);
-        }
-        catch (IllegalArgumentException e){
-            System.out.println(e.getMessage());
-        }
+
+
+    public void compare(){
+
     }
 }
--- src/main/java/bridge/Controller/BridgeControllerException.java
+++ src/main/java/bridge/Controller/BridgeControllerException.java
@@ -6,6 +6,17 @@ import java.lang.invoke.ConstantBootstraps;
 
 public class BridgeControllerException {
 
+    public static void bridgeValidateCheck(String input){
+        try {
+            numberCheck(input);
+            rangeCheck(input);
+        }
+        catch (NumberFormatException e){
+            System.out.println(e.getMessage());
+        }catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
+    }
     public static void numberCheck(String inputString){ //입력받은 string이 숫자형식인지 체크하는 함수
         try {
             Double.parseDouble(inputString);
@@ -20,7 +31,15 @@ public class BridgeControllerException {
             throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
         }
     }
-    
+
+    public static void movingValidateCheck(String input){
+        try {
+            alphabetCheck(input);
+        }
+        catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
+    }
     public static void alphabetCheck(String inputString){
         if(!inputString.equals("D")&&!inputString.equals("U")){
             throw new IllegalArgumentException("[ERROR] 이동할 칸은 U 또는 D로 입력해주세요");
