bdb3451ee4d72c95fdcbc09e08748177566f8dbb
Yeonwoo <yoanwoo0227@gmail.com>
2023-09-09 17:13:35 +0900

    feat : 상수 클래스 Constants 구현
    다리의 최소 길이 BRIDGE_MIN_NUMBER=3, 다리의 최대 길이 BRIDGE_MAX_NUMBER=20, 위 칸을 의미하는 UP="U", 아래칸을 의미하는 DOWN="D"로 구현하였다.

---
+++ src/main/java/bridge/Constants.java
@@ -0,0 +1,9 @@
+package bridge;
+
+public class Constants {
+    public final static int BRIDGE_MIN_NUMBER=3;
+    public final static int BRIDGE_MAX_NUMBER=20;
+    public final static String UP="U";
+    public final static String DOWN="D";
+
+}
