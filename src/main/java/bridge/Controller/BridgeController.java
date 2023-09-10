fc0f20a2414463af5d3c5ba5e4786ace8a2fba78
Yeonwoo <yoanwoo0227@gmail.com>
2023-09-10 14:36:33 +0900

    feat : BridgeController클래스에 재시작 여부를 확인하는 getRetry()구현
    이 메소드가 호출되었다는 뜻은 사용자의 재시작 여부를 입력받고 전달해야 한다는 뜻이므로 InputView의 readGameCommand()로 입력을 받아오고 예외클래스 BridgeControllerException()의 retryAlphabetCheck()로 R또는 Q를 입력받았는지 체크한다. 제대로 받았다면 R값이라면 재시작을 뜻하는 true를, Q라면 false를 리턴한다.

--- src/main/java/bridge/Controller/BridgeController.java
+++ src/main/java/bridge/Controller/BridgeController.java
@@ -1,6 +1,5 @@
 package bridge.Controller;
 
-import bridge.BridgeGame;
 import bridge.BridgeMaker;
 import bridge.BridgeNumberGenerator;
 import bridge.BridgeRandomNumberGenerator;
@@ -16,7 +15,6 @@ public class BridgeController {
     OutputView outputView=new OutputView();
     Bridge bridge=new Bridge();
     BridgeNumberGenerator bridgeNumberGenerator=new BridgeRandomNumberGenerator();
-    BridgeGame bridgeGame=new BridgeGame();
     private int trialNum=0;
     public BridgeController() {
 
@@ -39,9 +37,33 @@ public class BridgeController {
     public void compareToBridge(String move){
         int moveIndex=bridge.getCheckIndex(); //이번에 사용자가 이동할 칸의 인덱스 값
         if(move.equals(bridge.getMadeBridge().get(moveIndex))){
-           bridge.setCheckedBridge("O"); //이동할 칸과 비교해서 같으면 통과를 뜻하는 "O"를 Bridge클래스의 setCheckedBridge에 전달한다.
+           bridge.setCheckedBridge("O",move); //이동할 칸과 비교해서 같으면 통과를 뜻하는 "O"를 Bridge클래스의 setCheckedBridge에 전달한다.
             return;
         }
-        bridge.setCheckedBridge("X"); ////다르면 실패를 뜻하는 "X"를 Bridge클래스의 setCheckedBridge에 전달한다.
+        bridge.setCheckedBridge("X",move); ////다르면 실패를 뜻하는 "X"를 Bridge클래스의 setCheckedBridge에 전달한다.
+    }
+
+    public void printBridge(){
+        outputView.printMap(bridge.getCheckedUpBridge(), bridge.getCheckedDownBridge());
+    }
+
+    public String getLastResult(){ //결과 리스트의 마지막 결과를 가져온다.
+        int lastIndex=bridge.getCheckIndex()-1; //가장 마지막으로 체크한 칸의 인덱스
+        return bridge.getMadeBridge().get(lastIndex);
+    }
+
+    public boolean isFail(){
+        if(getLastResult()=="X"){ //마지막 결과가 실패이면 실패를 뜻하는 true 리턴
+            return true;
+        }
+        return false;
+    }
+    public boolean getRetry(){
+        String retry=inputView.readGameCommand();
+        BridgeControllerException.retryAlphabetCheck(retry);
+        if(retry.equals("R")){
+            return true;
+        }
+        return false;
     }
 }
