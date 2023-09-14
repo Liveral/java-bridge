59c7b1e97f3b563f57798c07b7848ba5a0715fa2
Yeonwoo <yoanwoo0227@gmail.com>
2023-09-14 19:34:02 +0900

    feat : Outview클래스에서 다리의 상태를 출력하는 printMap() 구현
    매개변수로 위쪽 다리와 아래쪽 다리의 현 상태를 받는다. 그리고 기본으로 출력되는 벽과 OX를 출력하고 가운데 들어가는 결과는 따로 처리해야 하므로 additionPrint()함수를 만들어 처리해주었다.

--- src/main/java/bridge/View/OutputView.java
+++ src/main/java/bridge/View/OutputView.java
@@ -1,15 +1,18 @@
 package bridge.View;
 
 import bridge.Model.Bridge;
+import bridge.ResultEnum;
+
+import java.util.List;
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
 
-    private final String LEFT_WALL="[";
-    private final String RIGHT_WALL="]";
-    private final String DIVIDE_WALL="|";
+    private final String LEFT_WALL="[ ";
+    private final String RIGHT_WALL=" ]";
+    private final String DIVIDE_WALL=" | ";
     private final String MOVE_SUCCESS="O";
     private final String MOVE_FAIL="X";
 
@@ -25,16 +28,32 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap(boolean result, int moveIndex) {
-
+    public void printMap(List<String> upBridge, List<String> downBridge) {
+        System.out.print(LEFT_WALL);
+        System.out.print(upBridge.get(0));
+        additionPrint(upBridge);
+        System.out.println(RIGHT_WALL);
+        System.out.print(LEFT_WALL);
+        System.out.print(downBridge.get(0));
+        additionPrint(downBridge);
+        System.out.println(RIGHT_WALL);
     }
+
+
 
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
-
+    public void printResult(String result,int trialNumber) {
+        System.out.print("게임 성공 여부: ");
+        System.out.println(ResultEnum.valueOf(result).getResult());
+        System.out.print("총 시도한 횟수: ");
+        System.out.println(trialNumber);
     }
+    public void printResultMessage(){
+        System.out.println("최종 게임 결과");
+    }
+
 }
