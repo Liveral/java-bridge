6c0cb86826d6c0151abbfa4075285e0197aabbc4
Yeonwoo <yoanwoo0227@gmail.com>
2023-09-14 19:24:15 +0900

    feat : BridgeGame클래스에 playerMove() 구현
    사용자가 움직이고 움직일때마다 결과를 출력하고, 실패 성공 여부를 체크하는 행동이 모두 집합된 함수이다. 무한반복 while문 안에서 move()와 print()를 반복하면서 BridgeController의 isFail()를 체크하고 실패라면 재시도 여부를 확인하는 retry()함수를 호출. 실패가 아니라면 성공인지 체크하고 성공이면 while문 종료.

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,16 +1,52 @@
 package bridge;
 
+import bridge.Controller.BridgeController;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
 
+    BridgeController bridgeController=new BridgeController();
+    public BridgeGame() {
+        try {
+            bridgeController.bridgeMake(); // 입력받은 다리의 길이만큼 다리를 만들어주는 bridgeMake()를 호출해 다리 생성
+        }
+        catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void playGame(){
+        bridgeController.addTrialNum();
+        playerMove(); //사용자는 게임이 종료될때까지 움직인다.
+        bridgeController.printGameResult();
+
+    }
+    public void playerMove(){
+        while(true){
+            move();
+            print();
+            if(bridgeController.isFail()){
+                retry();
+                break;
+            }
+            if(bridgeController.isSuccess()){
+                break;
+            }
+        }
+    }
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void move() {
+        bridgeController.userMove();
+
+    }
+    public void print(){
+        bridgeController.printBridge();
     }
 
     /**
@@ -19,5 +55,11 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+        if(bridgeController.getRetry()){ //사용자가 R을 입력해 재시작을 원할 경우
+            bridgeController.clearResult();
+            bridgeController.addTrialNum();
+            playerMove();
+        }
+
     }
 }
